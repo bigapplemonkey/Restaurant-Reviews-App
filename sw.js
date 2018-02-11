c2347e4f9bdf605325b6b542dbc12de583ce7c68
Jorge Asuaje <jorge.asuaje@gmail.com>
2018-02-11 16:03:44 -0500

    Comment fetch debbug

--- sw.js
+++ sw.js
@@ -21,22 +21,22 @@ self.addEventListener('fetch', function(event) {
         caches.open(staticCacheName).then(function(cache) {
             return cache.match(event.request).then(function(response) {
                 //Uncomment this for caching debugging
-                if (response) {
-                    console.log(`Getting from cache ${event.request.url}`);
-                    return response;
-                } else {
-                    console.log(`Getting from network ${event.request.url}`);
-                    return fetch(event.request).then(function(response) {
-                        console.log(`Adding to cache ${event.request.url}`);
-                        cache.put(event.request, response.clone());
-                        return response;
-                    });
-                }
-
-                // return response || fetch(event.request).then(function(response) {
-                //     cache.put(event.request, response.clone());
+                // if (response) {
+                //     console.log(`Getting from cache ${event.request.url}`);
                 //     return response;
-                // });
+                // } else {
+                //     console.log(`Getting from network ${event.request.url}`);
+                //     return fetch(event.request).then(function(response) {
+                //         console.log(`Adding to cache ${event.request.url}`);
+                //         cache.put(event.request, response.clone());
+                //         return response;
+                //     });
+                // }
+
+                return response || fetch(event.request).then(function(response) {
+                    cache.put(event.request, response.clone());
+                    return response;
+                });
             });
         })
     );
