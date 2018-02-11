ce0f5da562e995d744ae4e375895598a797a73c3
Jorge Asuaje <jorge.asuaje@gmail.com>
2018-02-11 15:35:09 -0500

    Update service worker path

--- js/main.js
+++ js/main.js
@@ -16,7 +16,8 @@ document.addEventListener('DOMContentLoaded', (event) => {
 
 registerServiceWorker = () => {
         if ('serviceWorker' in navigator) {
-            navigator.serviceWorker.register('/sw.js').then((reg) => {
+            navigator.serviceWorker.register('sw.js').then((reg) => {
+                console.log('ServiceWorker registration successful with scope: ', registration.scope);
                 if (reg.installing) {
                     console.log('Service worker installing');
                 } else if (reg.waiting) {
