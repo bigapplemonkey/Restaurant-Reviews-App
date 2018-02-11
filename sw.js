b0452423e5fbca4b3d6d7116b092f84e9fe297a6
Jorge Asuaje <jorge.asuaje@gmail.com>
2018-02-11 15:48:54 -0500

    Update service worker

--- sw.js
+++ sw.js
@@ -5,12 +5,12 @@ self.addEventListener('install', function(event) {
     event.waitUntil(
         caches.open(staticCacheName).then(function(cache) {
             return cache.addAll([
-                '/index.html',
-                '/js/dbhelper.js',
-                '/js/main.js',
-                '/js/restaurant_info.js',
-                '/css/styles.css',
-                '/data/restaurants.json'
+                'index.html',
+                'js/dbhelper.js',
+                'js/main.js',
+                'js/restaurant_info.js',
+                'css/styles.css',
+                'data/restaurants.json'
             ]);
         })
     );
