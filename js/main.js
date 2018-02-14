58c165519cbd0f687b0029381d6bf6bedc8721dd
Jorge Asuaje <jorge.asuaje@gmail.com>
2018-02-14 12:58:11 -0500

    Add more ARIA attributes / Improve responsiveness for viewports larger than 1400px

--- js/main.js
+++ js/main.js
@@ -169,19 +169,23 @@ createRestaurantHTML = (restaurant) => {
     image.setAttribute('alt', `${restaurant.name} Restaurant`);
     li.append(image);
 
-    const name = document.createElement('h1');
+    const name = document.createElement('h2');
+    name.className = 'restaurant-name';
     name.innerHTML = restaurant.name;
     li.append(name);
 
     const neighborhood = document.createElement('p');
     neighborhood.innerHTML = restaurant.neighborhood;
+    neighborhood.className = 'restaurant-neighborhood';
     li.append(neighborhood);
 
     const address = document.createElement('p');
+    address.className = 'restaurant-address';
     address.innerHTML = restaurant.address;
     li.append(address);
 
     const more = document.createElement('a');
+    more.className = 'restaurant-link';
     more.innerHTML = 'View Details';
     more.href = DBHelper.urlForRestaurant(restaurant);
     li.append(more)
